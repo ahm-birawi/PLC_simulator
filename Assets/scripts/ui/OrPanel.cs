ede2ebf0f9047d82c7ac128be13b1a44773d8f67
ahm-birawi <ahm.birawi@gmail.com>
2021-06-15 12:27:01 +0300

    Remaking the OR UI

---
+++ Assets/scripts/ui/OrPanel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrPanel : MonoBehaviour
+{
+    public int height = 77;
+    public int height1=0;
+    public int width;
+    int panel1;
+    int panel2;
+    public Transform panel1T;
+    public Transform panel2T;
+    void Update()
+    {
+        panel1 = panel1T.childCount-1;
+        panel2 = panel2T.childCount-1;
+        if (transform.Find("Panel/minirung(Clone)") != null)
+        {
+            panel1 += panel1T.GetComponentInChildren<OrPanel>().width;
+            height1 = panel1T.GetComponentInChildren<OrPanel>().height;
+
+            
+        }
+        if (transform.Find("Panel (1)/minirung(Clone)") != null)
+        {
+            panel2 += panel2T.GetComponentInChildren<OrPanel>().width;
+
+        }
+        if (panel1 > panel2)
+            width = panel1;
+        else
+            width = panel2;
+        transform.GetComponent<RectTransform>().sizeDelta = new Vector2(50 + width * 50, height+height1/2 );
+        panel2T.localPosition = new Vector2(transform.Find("loweredge").localPosition.x, transform.Find("loweredge").localPosition.y);
+        Debug.Log(transform.GetComponent<RectTransform>().sizeDelta.y);
+        if (transform.parent.parent.name == "minirung(Clone)")
+        {
+            transform.localPosition = new Vector2(0, 0);
+        }
+
+    }
+}
--- Assets/scripts/ui/miniparallel_rung.cs
+++ Assets/scripts/ui/miniparallel_rung.cs
@@ -19,38 +19,7 @@ public class miniparallel_rung : MonoBehaviour
         }
 
 
-            if (this.transform.childCount >= this.transform.parent.GetChild(1).childCount)
-            {
-            for (int i = 0; i <= this.transform.childCount; i++)
-            {
-
-
-                int rectsizex = 55;
-
-                if (this.transform.childCount == i)
-                {
-                    RectTransform miniRectTransform = this.transform.GetComponent<RectTransform>();
-
-                    miniRectTransform.sizeDelta = new Vector2(rectsizex + (37 * (i - 1)), miniRectTransform.sizeDelta.y);
-
-
-                    if (this.transform.parent.childCount == 2)
-                    {
-                        RectTransform miniRectTransform2 = this.transform.parent.GetChild(1).GetComponent<RectTransform>();
-
-                        miniRectTransform2.sizeDelta = new Vector2(rectsizex + (37 * (i - 1)), miniRectTransform2.sizeDelta.y);
-                    }
-
-
-                    RectTransform miniRectTransformparent = this.transform.parent.GetComponent<RectTransform>();
-
-                    miniRectTransformparent.sizeDelta = new Vector2(rectsizex + 3 + (37 * (i - 1)), miniRectTransformparent.sizeDelta.y);
-
-
-                    break;
-                }
-            }
-        }
+         
     
 
 
